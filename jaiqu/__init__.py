041b65d842e5a517e9a9160e8d37421136999576
reibs <areibman@gmail.com>
2024-02-22 15:07:44 -0800

    update implementaiton notebook

--- jaiqu/__init__.py
+++ jaiqu/__init__.py
@@ -1,11 +1,1 @@
-from openai import OpenAI
 from .jaiqu import validate_schema, translate_schema
-
-
-class LLM:
-    openai_client = None
-
-    @classmethod
-    def init(cls, openai_api_key: str):
-        if cls.openai_client is None:
-            cls.openai_client = OpenAI(api_key=openai_api_key)
